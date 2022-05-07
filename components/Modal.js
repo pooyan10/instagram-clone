3f35e6bef8a10bd162d9aede292d34507d05315c
Pouyan Hamidi <pouyan@Pouyans-MacBook-Pro.local>
2022-05-07 12:55:08 +0430

    firestore posts

--- components/Modal.js
+++ components/Modal.js
@@ -1,10 +1,70 @@
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtoms";
 import { Dialog, Transition } from "@headlessui/react";
-import { Fragment } from "react";
+import { Fragment, useRef, useState } from "react";
 import { CameraIcon } from "@heroicons/react/outline";
+import { useSession } from "next-auth/react";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  updateDoc,
+  doc,
+} from "@firebase/firestore";
+import { ref, getDownloadURL, uploadString } from "@firebase/storage";
+import { db, storage } from "../firebase";
+
 function Modal() {
+  const { data: session } = useSession();
   const [open, setOpen] = useRecoilState(modalState);
+  const filePickerRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const captionRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+
+  const uploadPost = async () => {
+    if (loading) return;
+    setLoading(true);
+
+    // 1) Create a post and add to firestore 'posts' collection
+    // 2) get the post ID for the newly created post
+    // 3) upload the image to firebase storage with the post ID
+    // 4) get a download URL from fb storage and update the original post with image
+
+    const docRef = await addDoc(collection(db, "posts"), {
+      username: session.user.username,
+      caption: captionRef.current.value,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+
+    console.log("new doc added with ID", docRef.id);
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+
+    await uploadString(imageRef, selectedFile, "data_url").then(
+      async (snapshot) => {
+        const downloadURL = await getDownloadURL(imageRef);
+
+        await updateDoc(doc(db, "posts", docRef.id), {
+          image: downloadURL,
+        });
+      }
+    );
+    setOpen(false);
+    setLoading(false);
+    setSelectedFile(null);
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -42,12 +102,25 @@ function Modal() {
           >
             <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
               <div>
-                <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer">
-                  <CameraIcon
-                    className="h-6 w-6 text-red-600"
-                    aria-hidden="true"
+                {selectedFile ? (
+                  <img
+                    className="w-full object-contain cursor-pointer"
+                    src={selectedFile}
+                    onClick={() => setSelectedFile(null)}
+                    alt=""
                   />
-                </div>
+                ) : (
+                  <div
+                    onClick={() => filePickerRef.current.click()}
+                    className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
+                  >
+                    <CameraIcon
+                      className="h-6 w-6 text-red-600"
+                      aria-hidden="true"
+                    />
+                  </div>
+                )}
+
                 <div>
                   <div className="mt-3 text-center sm:mt-5">
                     <Dialog.Title
@@ -57,10 +130,16 @@ function Modal() {
                       Upload a photo
                     </Dialog.Title>
                     <div>
-                      <input type="file" hidden />
+                      <input
+                        onChange={addImageToPost}
+                        ref={filePickerRef}
+                        type="file"
+                        hidden
+                      />
                     </div>
                     <div className="mt-2">
                       <input
+                        ref={captionRef}
                         className="border-none focus:ring-0 w-full text-center"
                         type="text"
                         placeholder="please enter a caption..."
@@ -72,9 +151,11 @@ function Modal() {
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
+                    disabled={!selectedFile}
                     className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                    onClick={uploadPost}
                   >
-                    Upload Post
+                    {loading ? "Uploading..." : "Upload Post"}
                   </button>
                 </div>
               </div>
--- components/Posts.js
+++ components/Posts.js
@@ -1,33 +1,33 @@
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Post from "./Post";
 
-const post = [
-  {
-    id: "123",
-    username: "ali",
-    userImg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/4cj",
-    caption: "lets goooo baby",
-  },
-  {
-    id: "334",
-    username: "hamid",
-    userImg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/3ke",
-    caption: "say my name",
-  },
-];
-
 function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+
+    [db]
+  );
+
   return (
     <div className="">
-      {post.map(({ id, username, userImg, img, caption }) => (
+      {posts.map((post) => (
         <Post
-          key={id}
-          id={id}
-          username={username}
-          userImg={userImg}
-          img={img}
-          caption={caption}
+          key={post.id}
+          id={post.id}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
